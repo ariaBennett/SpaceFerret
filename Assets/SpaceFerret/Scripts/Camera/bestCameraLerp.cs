4d4a0dc0d86d257717670bebfa613d9be4be011b
Aria <aria@Projector.local>
2014-04-01 18:16:49 -0700

    Stable build; experimental features disabled.

---
+++ Assets/SpaceFerret/Scripts/Camera/bestCameraLerp.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using System.Collections;
+
+public class bestCameraLerp : MonoBehaviour {
+
+	public GameObject ship;
+	public Transform marker;
+
+	public float moveSpeed = 1;
+	public float rotateSpeed = 1;
+
+	public float offsetX = 0;
+	public float offsetY = 0;	
+	public float offsetZ = 0;
+
+	
+	
+	void Update () {
+	  Vector3 newPosition = new Vector3(
+			marker.position.x,
+			marker.position.y,
+			marker.position.z
+			);
+            transform.position = Vector3.Lerp(transform.position, newPosition, Time.deltaTime * moveSpeed);
+            transform.rotation = Quaternion.Lerp(transform.rotation, ship.transform.rotation, Time.deltaTime * rotateSpeed);
+	}
+}
--- Assets/SpaceFerret/Scripts/Movement.js
+++ Assets/SpaceFerret/Scripts/Movement.js
@@ -1,19 +1,97 @@
 ﻿#pragma strict
 
-var baseSpeed : float = 1000;
-var boostSpeed : float = 1500;
-var breakSpeed : float = 500;
-var speed : float;
+var leapRight: GameObject;
+
+var cameraMarker: GameObject;
+var cameraMarkerOffsetX: float = 40;
+var cameraMarkerOffsetY: float = 0;
+var cameraMarkerOffsetZ: float = 40;
+
+var rotationSpeed: float = 500;
+var curveBoost: float = 100;
+var cameraLookSpeed: float = 50;
+
+var baseSpeed: float = 1000;
+var boostSpeed: float = 1500;
+var breakSpeed: float = 500;
+var speed: float;
+var wasSpinning: boolean = false;
 
 
 
-function Start () {
 
-}
+function Update() {
 
-function Update () {
-  speed = baseSpeed;
-  this.transform.Translate(Vector3.forward * speed * Time.deltaTime);
-  //this.rigidbody.AddForce(this.transform.forward * speed);
+    // Rotate stuff
+    var currentRotation = this.transform.rotation;
+    var leapRotation = leapRight.transform.rotation;
 
-}
+    var clampedRotation: Vector3;
+
+    if (leapRotation.eulerAngles.z > 90 && leapRotation.eulerAngles.z < 270) {
+        if (leapRotation.eulerAngles.z <= 180) {
+          //if (wasSpinning === false) {
+          if (0) {
+            wasSpinning = true;
+            cameraMarker.transform.position = new Vector3(
+                cameraMarkerOffsetX,
+                cameraMarkerOffsetY,
+                cameraMarkerOffsetZ
+            );
+          }
+            
+            // this.transform.rotation = Quaternion.Lerp(this.transform.rotation, Quaternion.Euler(0, this.transform.rotation.y, this.transform.rotation.z), curveBoost * Time.deltaTime);
+            clampedRotation = new Vector3(
+                leapRotation.eulerAngles.x,
+                leapRotation.eulerAngles.y,
+                90
+            );
+        } 
+        else {
+            if (leapRotation.eulerAngles.z >= 180) {
+             if (0) {
+             // if (wasSpinning === false) {
+                wasSpinning = true;
+                cameraMarker.transform.position = new Vector3(-cameraMarkerOffsetX, -cameraMarkerOffsetY, -cameraMarkerOffsetZ);
+              }
+                
+                //Camera.main.transform.RotateAround(this.transform.position, Vector3.left, cameraLookSpeed);
+                //  this.transform.rotation = Quaternion.Lerp(this.transform.rotation, Quaternion.Euler(360, this.transform.rotation.y, this.transform.rotation.z), curveBoost * Time.deltaTime);
+                clampedRotation = new Vector3(
+                    leapRotation.eulerAngles.x,
+                    leapRotation.eulerAngles.y,
+                    270
+                );
+            }
+        } 
+    }
+    else {
+        if (wasSpinning == true) {
+         cameraMarker.transform.position = new Vector3(0, 0, -10);
+        }
+       
+        clampedRotation = new Vector3(
+            leapRotation.eulerAngles.x,
+            leapRotation.eulerAngles.y,
+            leapRotation.eulerAngles.z
+        );
+    }
+        /*
+      clampedRotation = new Vector3(
+      leapRotation.eulerAngles.x,
+      leapRotation.eulerAngles.y,
+      leapRotation.eulerAngles.z
+    );
+    */
+
+        var step = rotationSpeed * Time.deltaTime;
+        this.transform.rotation = Quaternion.RotateTowards(currentRotation, Quaternion.Euler(clampedRotation), step);
+
+
+        // Speed below
+
+        speed = baseSpeed;
+        this.transform.Translate(Vector3.forward * speed * Time.deltaTime);
+
+
+    }
